20defa4961a098f0cb4a110eaff4ba1870d81ad4
Krishnarjun Singh <krishnarjun.singh2015@gmail.com>
2020-05-02 23:54:09 +0530

    Creating project

---
+++ WebScratchLearning/src/main/java/com/WebScratch/WebScratchLearning/WebScratchLearningApplication.java
@@ -0,0 +1,13 @@
+package com.WebScratch.WebScratchLearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebScratchLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebScratchLearningApplication.class, args);
+	}
+
+}
---
+++ WebScratchLearning/src/test/java/com/WebScratch/WebScratchLearning/WebScratchLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.WebScratch.WebScratchLearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebScratchLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
